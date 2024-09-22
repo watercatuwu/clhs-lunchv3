5bae0fc4f3bfc6285949ee0a9f3259b8f93161d8
watercat <watercat0330@gmail.com>
2024-09-23 01:19:58 +0800

    del middleware

--- middleware/setManifestMIME.global.js
+++
@@ -1,6 +0,0 @@
-export default function (context) {
-    const { req, res } = context;
-    if (req.url === '/manifest.webmanifest') {
-      res.setHeader('Content-Type', 'application/manifest+json');
-    }
-  }
